78b648f4083c90b89b0e35a541f29bec20dcc0d9
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-06-16 17:01:34 +0700

    commit all

--- app/Views/kasir/page_kasir.php
+++ app/Views/kasir/page_kasir.php
@@ -402,7 +402,7 @@
         }
 
         const hargaAwal = $('#harga_awal').val();
-        const hargaAwalNumber = parseFloat(hargaAwal.replace(/,/g, ''));
+        const hargaAwalNumber = parseFloat(hargaAwal.replace(/./g, ''));
         $('#harga_akhir').val(thousandSeparator(hargaAwalNumber - inputedValue));
     });
 
