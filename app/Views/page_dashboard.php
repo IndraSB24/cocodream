3a79e583fdc853fca98d579ced0780d24a11ae0d
IndraSbudi <128012203+IndraSbudi@users.noreply.github.com>
2024-07-05 03:06:08 +0700

    all

--- app/Views/page_dashboard.php
+++ app/Views/page_dashboard.php
@@ -234,6 +234,18 @@
         <!-- apexcharts init -->
         <script src="assets/js/pages/apexcharts.init.js"></script>
 
+        <!-- jquery.vectormap map -->
+        <script src="assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js"></script>
+
+        <!-- Required datatable js -->
+        <script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+
+        <!-- Responsive examples -->
+        <script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
         <script src="assets/js/pages/dashboard.init.js"></script>
         <?= $this->include('partials/vendor-scripts') ?>
         <?= $this->include('partials/custom-page-scripts') ?>
